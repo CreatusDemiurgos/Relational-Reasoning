55966ede2ebac40f9bed1b71e910a5137ed8eb2d
CreatusDemiurgos <yaallahwithme97@gmail.com>
2024-01-09 18:57:44 +0100

    Added function that compares the shape based on their location. Problem to fix in the future:correct input if we display the same two shapes, Ideas:add color (e.g:red square west of blue square) or make it so we cant display the same object (might not be useful since it could help as a form of interference)

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const cntx = canvas.getContext("2d");
 
 const canvas2 = document.getElementById("canvas2");
 const cntx2 = canvas2.getContext("2d");
+let right;
+let left;
+let randomIndexPhrase;
+let phraseTypeChosen;
 
 const shapeGenerator = new ShapeGenerator();
 
@@ -20,11 +24,36 @@ const shapeGenerators = [
 const shapeName = ["triangle ", "square ", "rectangle ", "pentagon ", "hexagon ", "circle "];
 
 const twoCanvasComparisonPhrases = [
-    "is lighter ",
-    "is darker ",
-    "is bigger ",
-    "is smaller ",
+        "is west of ",
+        "is east of ",
+        "is at the left of ",
+        "is at the right of "        
 ];
+
+function comparisonPosition(buttonClicked){
+    //checking if the phrase stated is ture or false. If it is true then its equal to 1, if its false its equal to 2
+    let statementVeracity;
+    if(randomIndexPhrase === 0 || randomIndexPhrase === 2){
+        if(phraseTypeChosen === 1){
+            statementVeracity = 1;
+        }else if(phraseTypeChosen === 2){
+            statementVeracity = 2;
+        }
+    }else if(randomIndexPhrase === 1 || randomIndexPhrase === 3){
+        if(phraseTypeChosen === 1){
+            statementVeracity = 2;
+        }else if(phraseTypeChosen === 2){
+            statementVeracity = 1;
+        }
+    }
+
+    if(buttonClicked === statementVeracity){
+        console.log("CORRECT!");
+    }else{
+        console.log("FALSE!");
+    }
+    debugger;
+}
 
 
 
@@ -32,16 +61,32 @@ const twoCanvasComparisonPhrases = [
 function displayRandomShape(){
     cntx.clearRect(0, 0, canvas.width, canvas.height);
     const randomIndex = Math.floor(Math.random() * shapeGenerators.length);
+    left = randomIndex;
     shapeGenerators[randomIndex](cntx);
     cntx2.clearRect(0, 0, canvas2.width, canvas2.height);
     const randomIndex2 = Math.floor(Math.random() * shapeGenerators.length);
+    right = randomIndex2;
     shapeGenerators[randomIndex2](cntx2);
-    const randomIndexPhrase = Math.floor(Math.random() * twoCanvasComparisonPhrases.length);
-    var finalPhrase = shapeName[randomIndex] + twoCanvasComparisonPhrases[randomIndexPhrase] + shapeName[randomIndex2]
+    randomIndexPhrase = Math.floor(Math.random() * twoCanvasComparisonPhrases.length);
+    const randomPhraseTypeChooser = Math.floor(Math.random());
+    let finalPhrase;
+    if(randomPhraseTypeChooser === 0){
+        finalPhrase = shapeName[randomIndex] + twoCanvasComparisonPhrases[randomIndexPhrase] + shapeName[randomIndex2];
+        phraseTypeChosen = 1;
+    }else{
+        finalPhrase = shapeName[randomIndex2] + twoCanvasComparisonPhrases[randomIndexPhrase] + shapeName[randomIndex];
+        phraseTypeChosen = 2;
+    }
     var paragraph = document.getElementById("statement");
     paragraph.textContent = finalPhrase;
 }
 
+function handleButtonClick(buttonClicked){
+    comparisonPosition(buttonClicked);
+    displayRandomShape();
+    
+}
 
-document.getElementById("falseBtn").addEventListener("click", displayRandomShape);
-document.getElementById("trueBtn").addEventListener("click", displayRandomShape);
+
+document.getElementById("falseBtn").addEventListener("click", function(){handleButtonClick(2)});
+document.getElementById("trueBtn").addEventListener("click", function(){handleButtonClick(1)}); 
