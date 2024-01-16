d0f832256ead61347e3a8059ee805ef545b0b4ed
CreatusDemiurgos <yaallahwithme97@gmail.com>
2024-01-16 16:15:20 +0100

    Added inside outside comparison on the third, central, canvas

--- functions/shapeGenerator.js
+++ functions/shapeGenerator.js
@@ -8,42 +8,15 @@ class ShapeGenerator {
         return color;
     }
 
-    makeTriangle(ctx, containerWidth, containerHeight) {
+    generateRegularPols(ctx, containerWidth, containerHeight,sizeChager, sides){
         let randColor = this.getRandomColor();
         ctx.beginPath();
-        ctx.moveTo(containerWidth / 4, containerHeight / 4);
-        ctx.lineTo((3 * containerWidth) / 4, containerHeight / 4);
-        ctx.lineTo(containerWidth / 2, (3 * containerHeight) / 4);
-        ctx.closePath();
-        ctx.fillStyle = randColor;
-        ctx.fill();
-    }
-
-    makeSquare(ctx, containerWidth, containerHeight) {
-        let randColor = this.getRandomColor();
-        ctx.beginPath();
-        ctx.rect(containerWidth / 4, containerHeight / 4, containerWidth / 2, containerHeight / 2);
-        ctx.fillStyle = randColor;
-        ctx.fill();
-    }
-
-    makeRectangle(ctx, containerWidth, containerHeight) {
-        let randColor = this.getRandomColor();
-        ctx.beginPath();
-        ctx.rect(containerWidth / 8, 1*containerHeight / 4, 3*containerWidth / 4, containerHeight / 2);
-        ctx.fillStyle = randColor;
-        ctx.fill();
-    }
-
-    makePentagon(ctx, containerWidth, containerHeight) {
-        let randColor = this.getRandomColor();
-        const sideLength = containerWidth/3.5;
-        const centerX = containerWidth /2;
-        const centerY = containerHeight/2;
-        const pentagonDegree = 108*(Math.PI/180);
-        ctx.beginPath();
-        for(let i = 0; i < 5; i++){
-            const angle = (1/2)*Math.PI + (i * 2 * Math.PI)/5;
+        const sideLength = containerWidth / (4 * sizeChager);
+        const centerX = containerWidth / 2;
+        const centerY = containerHeight /2;
+    
+        for (let i = 0; i < sides; i++) {
+            const angle = (i * 2 * Math.PI) / sides;
             const x = centerX + sideLength * Math.cos(angle);
             const y = centerY + sideLength * Math.sin(angle);
             ctx.lineTo(x, y);
@@ -53,29 +26,36 @@ class ShapeGenerator {
         ctx.fill();
     }
 
-
-    makeHexagon(ctx, containerWidth, containerHeight) {
-    let randColor = this.getRandomColor();
-    ctx.beginPath();
-    const sideLength = containerWidth / 4; // Adjust as needed
-    const centerX = containerWidth / 2;
-    const centerY = containerHeight / 2;
-
-    for (let i = 0; i < 6; i++) {
-        const angle = (i * 2 * Math.PI) / 6;
-        const x = centerX + sideLength * Math.cos(angle);
-        const y = centerY + sideLength * Math.sin(angle);
-        ctx.lineTo(x, y);
+    // the size is a value that is used when we want a shape to be inside another one, so we want the inner shapes to be smaller
+    makeTriangle(ctx, containerWidth, containerHeight, sizeChager) {
+        this.generateRegularPols(ctx, containerWidth, containerHeight,sizeChager, 3);
     }
-    ctx.closePath();
-    ctx.fillStyle = randColor;
-    ctx.fill();
-}
 
-    makeCircle(ctx, containerWidth, containerHeight) {
+    makeSquare(ctx, containerWidth, containerHeight, sizeChager) {
+        this.generateRegularPols(ctx, containerWidth, containerHeight,sizeChager, 4);
+    }
+
+    makeRectangle(ctx, containerWidth, containerHeight, sizeChager) {
         let randColor = this.getRandomColor();
         ctx.beginPath();
-        ctx.arc(containerWidth / 2, containerHeight / 2, containerWidth / 2.5, 0, 2 * Math.PI);
+        ctx.rect((containerWidth*sizeChager) / (8), ((containerHeight) / (2))- (containerHeight)/(4*sizeChager), 3*containerWidth / (4 * sizeChager), containerHeight / (2 * sizeChager));
+        ctx.fillStyle = randColor;
+        ctx.fill();
+    }
+
+    makePentagon(ctx, containerWidth, containerHeight, sizeChager) {
+        this.generateRegularPols(ctx, containerWidth, containerHeight,sizeChager, 5);
+    }
+
+
+    makeHexagon(ctx, containerWidth, containerHeight, sizeChager) {
+        this.generateRegularPols(ctx, containerWidth, containerHeight,sizeChager, 6);
+}
+
+    makeCircle(ctx, containerWidth, containerHeight, sizeChager) {
+        let randColor = this.getRandomColor();
+        ctx.beginPath();
+        ctx.arc(containerWidth / 2, containerHeight / 2, containerWidth / (2.5 * sizeChager), 0, 2 * Math.PI);
         ctx.fillStyle = randColor;
         ctx.fill();
     }
--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const cntx = canvas.getContext("2d");
 const canvas2 = document.getElementById("canvas2");
 const cntx2 = canvas2.getContext("2d");
 
+const canvas3 = document.getElementById("canvas3");
+const cntx3 = canvas3.getContext("2d");
+
 const tableScore = document.getElementById("score");
 const tableName = document.getElementById("name");
 let score = 0;
@@ -13,7 +16,9 @@ let score = 0;
 let right;
 let left;
 let randomIndexPhrase;
+let randomIndexOneCanvas;
 let phraseTypeChosen;
+let displayRandomComparison;
 
 const shapeGenerator = new ShapeGenerator();
 
@@ -32,26 +37,71 @@ const twoCanvasComparisonPhrases = [
         "is west of ",
         "is east of ",
         "is to the left of ",
-        "is to the right of "        
+        "is to the right of ",
+        "is not east of ",
+        "is not west of ",
+        "is not to the right of",
+        "is not the left of "      
 ];
+
+const oneCanvasComparisonPhrases = [
+    "is inside of ",
+    "is outside of ",
+    "is not outside of ",
+    "is not inside of ",
+    "is englobed by ",
+    "is englobing ",
+    "is encapsulated by ",
+    "is encapsulating ",
+    "is bounded by ",
+    "is bounding ",
+    "is not encompassing ",
+    "is not encompassed by"
+];
+
+
+function comparisons(buttonClicked){
+    let statementVeracity;
+    if(displayRandomComparison === 0 || displayRandomComparison === 1){
+        if((randomIndexOneCanvas % 2) === 0){
+            if(phraseTypeChosen === 1){
+                statementVeracity = 1;
+            }else if(phraseTypeChosen === 2){
+                statementVeracity = 2;
+            }
+        }else if((randomIndexOneCanvas % 2) === 1){
+            if(phraseTypeChosen === 1){
+                statementVeracity = 2;
+            }else if(phraseTypeChosen === 2){
+                statementVeracity = 1;
+            }
+        }
+    }
+    if(buttonClicked === statementVeracity){
+        score += 1;
+        tableScore.textContent = score;
+    }else{
+        score -= 1;
+        tableScore.textContent = score;
+    }
+}
 
 function comparisonPosition(buttonClicked){
     //checking if the phrase stated is ture or false. If it is true then its equal to 1, if its false its equal to 2
     let statementVeracity;
-    if(randomIndexPhrase === 0 || randomIndexPhrase === 2){
+    if((randomIndexPhrase % 2) === 0){
         if(phraseTypeChosen === 1){
             statementVeracity = 1;
         }else if(phraseTypeChosen === 2){
             statementVeracity = 2;
         }
-    }else if(randomIndexPhrase === 1 || randomIndexPhrase === 3){
+    }else if((randomIndexPhrase % 2) === 1){
         if(phraseTypeChosen === 1){
             statementVeracity = 2;
         }else if(phraseTypeChosen === 2){
             statementVeracity = 1;
         }
     }
-
     if(buttonClicked === statementVeracity){
         score += 1;
         tableScore.textContent = score;
@@ -62,26 +112,42 @@ function comparisonPosition(buttonClicked){
 }
 
 function displayRandomShape(){
-    cntx.clearRect(0, 0, canvas.width, canvas.height);
+    displayRandomComparison = Math.floor(Math.random() * 2);
     const randomIndex = Math.floor(Math.random() * shapeGenerators.length);
-    left = randomIndex;
-    shapeGenerators[randomIndex](cntx, canvas.width, canvas.height);
-    cntx2.clearRect(0, 0, canvas2.width, canvas2.height);
     const randomIndex2 = Math.floor(Math.random() * shapeGenerators.length);
-    right = randomIndex2;
-    shapeGenerators[randomIndex2](cntx2, canvas2.width, canvas2.height);
-    randomIndexPhrase = Math.floor(Math.random() * twoCanvasComparisonPhrases.length);
-    const randomPhraseTypeChooser = Math.floor(Math.random());
+    const randomPhraseTypeChooser = Math.floor(Math.random() * 2);
     let finalPhrase;
-    if(randomPhraseTypeChooser === 0){
-        finalPhrase = shapeName[randomIndex] + twoCanvasComparisonPhrases[randomIndexPhrase] + shapeName[randomIndex2];
-        phraseTypeChosen = 1;
-    }else{
-        finalPhrase = shapeName[randomIndex2] + twoCanvasComparisonPhrases[randomIndexPhrase] + shapeName[randomIndex];
-        phraseTypeChosen = 2;
+    if(displayRandomComparison === 0){
+        cntx.clearRect(0, 0, canvas.width, canvas.height);
+        cntx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        cntx3.clearRect(0, 0, canvas3.width, canvas3.height);
+        shapeGenerators[randomIndex](cntx, canvas.width, canvas.height, 1);
+        shapeGenerators[randomIndex2](cntx2, canvas2.width, canvas2.height, 1);
+        randomIndexPhrase = Math.floor(Math.random() * twoCanvasComparisonPhrases.length);
+        if(randomPhraseTypeChooser === 0){
+            finalPhrase = shapeName[randomIndex] + twoCanvasComparisonPhrases[randomIndexPhrase] + shapeName[randomIndex2];
+            phraseTypeChosen = 1;
+        }else{
+            finalPhrase = shapeName[randomIndex2] + twoCanvasComparisonPhrases[randomIndexPhrase] + shapeName[randomIndex];
+            phraseTypeChosen = 2;
+        }
+    }else if(displayRandomComparison === 1){
+        cntx.clearRect(0, 0, canvas.width, canvas.height);
+        cntx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        cntx3.clearRect(0, 0, canvas3.width, canvas3.height);
+        shapeGenerators[randomIndex](cntx3, canvas3.width, canvas3.height, 1);
+        shapeGenerators[randomIndex2](cntx3, canvas3.width, canvas3.height, 3);
+        randomIndexOneCanvas = Math.floor(Math.random() * oneCanvasComparisonPhrases.length);
+        if(randomPhraseTypeChooser === 0){
+            finalPhrase = shapeName[randomIndex2] + oneCanvasComparisonPhrases[randomIndexOneCanvas] + shapeName[randomIndex];
+            phraseTypeChosen = 1;
+        }else{
+            finalPhrase = shapeName[randomIndex] + oneCanvasComparisonPhrases[randomIndexOneCanvas] + shapeName[randomIndex2];
+            phraseTypeChosen = 2;
+        }
     }
     var paragraph = document.getElementById("statement");
-    paragraph.textContent = finalPhrase;
+    paragraph.textContent = finalPhrase;    
 }
 
 
@@ -97,7 +163,7 @@ tableName.textContent = userInput;
 function handleButtonClick(buttonClicked){
     //the comparison function NEED to be before display random shapes. This is due to the fact that we need to first compare
     //the displayed objects, then generate new ones.
-    comparisonPosition(buttonClicked);
+    comparisons(buttonClicked);
     displayRandomShape();
     
 }
